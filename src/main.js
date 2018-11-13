a65168a03d6fe3d6be0ae8e1d2dda4822c8ec714
Bitti09 <bitti06@gmail.com>
2018-11-13 22:22:06 +0100

    Add Vuetify

--- src/main.js
+++ src/main.js
@@ -1,9 +1,13 @@
+import '@babel/polyfill'
 import Vue from 'vue'
+import './plugins/vuetify'
 import App from './App.vue'
 import router from './router'
 import store from './store'
 import './registerServiceWorker'
 import { createProvider } from './vue-apollo'
+import 'roboto-fontface/css/roboto/roboto-fontface.css'
+import '@mdi/font/css/materialdesignicons.css'
 
 Vue.config.productionTip = false
 
