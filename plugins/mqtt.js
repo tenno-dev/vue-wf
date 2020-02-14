dfa8be94664092db598392b8a619c4eeceb7369a
Bitti <Bitti09@users.noreply.github.com>
2020-02-14 17:21:29 +0100

    Update mqtt.js

--- plugins/mqtt.js
+++ plugins/mqtt.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
 import VueMqtt from 'vue-mqtt-topics'
 // Vue.use(VueMqtt, 'wss://webs.mybitti.de:8084/mqtt')
-Vue.use(VueMqtt, 'ws://127.0.0.1:8083/mqtt')
+Vue.use(VueMqtt, 'ws://88.99.36.215:8083/mqtt')
