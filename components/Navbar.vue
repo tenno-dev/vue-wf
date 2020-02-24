fac60116ab48ed79940201f8c9b571285b2d4a62
Bitti <Bitti09@users.noreply.github.com>
2020-02-24 17:16:24 +0100

    fix  mobile style

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,32 +1,14 @@
 <template>
   <nav class=" flex items-center flex-wrap bg-navbar px-2 py-2">
-    <div class="flex items-center flex-no-shrink text-white mr-6">
-      <span class="font-semibold text-xl tracking-tight"
+    <div class="flex flex-shrink xs:flex-grow text-white mr-6">
+      <span class="static font-semibold text-xl tracking-tight"
         >Warframe Info Hub</span
       >
     </div>
-    <div class="mr-1 px-2 flex-grow w-80 number-col">
-      <v-select
-        :value="$store.state.activetheme"
-        :searchable="false"
-        :clearable="false"
-        :filterable="false"
-        :v-model="$store.state.activetheme"
-        :options="$store.state.theme"
-        @input="setSelectedtheme"
-      >
-        <template slot="option" slot-scope="option">
-          {{ option.label }}
-        </template>
-        <template slot="selected-option" slot-scope="option">
-          {{ option.label }}
-        </template></v-select
-      >
-    </div>
-    <div class="block sm:hidden mr-4">
+    <div class="sm:hidden static inset-y-0 right-0">
       <button
-        aria-label="Switch between Dark and Light theme"
-        class="flex items-center px-3 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
+        aria-label="Open Nav drawer"
+        class="flex items-center p-3 border rounded text-teal-lighter border-teal-light hover:text-white hover:border-white"
         @click="toggle"
       >
         <fa icon="bars" fixed-width class="fill-current text-primary" />
@@ -36,6 +18,24 @@
       :class="open ? 'block' : 'hidden'"
       class="flex-grow sm:flex sm:items-center sm:w-auto"
     >
+      <div class="mr-1 px-2   w-full number-col">
+        <v-select
+          :value="$store.state.activetheme"
+          :searchable="false"
+          :clearable="false"
+          :filterable="false"
+          :v-model="$store.state.activetheme"
+          :options="$store.state.theme"
+          @input="setSelectedtheme"
+        >
+          <template slot="option" slot-scope="option">
+            {{ option.label }}
+          </template>
+          <template slot="selected-option" slot-scope="option">
+            {{ option.label }}
+          </template></v-select
+        >
+      </div>
       <div class="mr-1 px-2 w-40 number-col">
         <v-select
           :value="$store.state.activeplatform"
@@ -92,12 +92,12 @@
 .number-col {
   font-family: 'Roboto', sans-serif !important;
   max-width: 14rem !important;
-  min-width: 10rem !important;
+  min-width: 9rem !important;
 }
 
 .vs__dropdown-menu {
   max-width: 11rem !important;
-  min-width: 11rem !important;
+  min-width: 5rem !important;
 
   @apply pb-1;
   @apply bg-navbar;
