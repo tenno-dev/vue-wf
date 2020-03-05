0faeadc16d7beed92a54488e954752b3d7125e65
Bitti <Bitti09@users.noreply.github.com>
2020-03-05 18:55:19 +0100

    update  vue-mqtt
    Use version  from https://github.com/Lurow/Vue-Mqtt
    
    with updated mqtt core

--- plugins/mqtt.js
+++ plugins/mqtt.js
@@ -1,4 +1,3 @@
+import VueMqtt from 'vue-mqtt'
 import Vue from 'vue'
-import VueMqtt from 'vue-mqtt-topics'
-// Vue.use(VueMqtt, 'wss://webs.mybitti.de:8084/mqtt')
 Vue.use(VueMqtt, 'wss://api-wf.mybitti.de:8084/mqtt')
